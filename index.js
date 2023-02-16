55875374688405330e9418fd21b38204149d344f
Alicia Akdogan <alicia.akdogan@hotmail.com>
2023-02-16 12:09:12 +0100

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+import Express from "express";
+const app = Express();
+import { config } from "dotenv";
+config();
+
+import { createOrder } from "./klarna.js";
+
+const products = [
+  { id: "1", name: "Chair", price: 57 },
+  { id: "2", name: "Table", price: 12 },
+  { id: "3", name: "House", price: 89 },
+];
+
+app.get("/", (req, res) => {
+  res.send(
+    products
+      .map((product) => `<a href="/p/${product.id}">${product.name}</a>`)
+      .join("")
+  );
+});
+
+app.get("/p/:id", async (req, res) => {
+  const product = products.find((product) => product.id === req.params.id);
+  const data = await createOrder(product);
+  res.send(data.html_snippet);
+});
+
+app.listen(3000);
